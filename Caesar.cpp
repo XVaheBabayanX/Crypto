870f236c37e570c927469bae372a7858dc7a4087
Vahe Babayan <vahebabayan.tt219-2@polytechnic.am>
2024-10-01 21:13:44 +0400

    Add files via upload

---
+++ Caesar.cpp
@@ -0,0 +1,47 @@
+﻿#include "Caesar.h"
+
+
+CaesarCipher::CaesarCipher(int s) : shift(s)
+{
+    Key.resize(ASCII); 
+    Shift(shift);
+}
+
+void CaesarCipher::Encrypt(const std::string& OriginalText, std::string& EncryptedText)
+{
+    int text_length = OriginalText.length();
+    EncryptedText.resize(text_length);
+
+    for (int i = 0; i < text_length; i++)
+    {
+        EncryptedText[i] = Key[static_cast<unsigned char>(OriginalText[i])];
+    }
+}
+
+void CaesarCipher::Decrypt(const std::string& EncryptedText, std::string& DecryptedText)
+{
+    int text_length = EncryptedText.length();
+    DecryptedText.resize(text_length);
+
+    for (int i = 0; i < text_length; i++)
+    {
+        for (int j = 0; j < ASCII; j++)
+        {
+            if (EncryptedText[i] == Key[j])
+            {
+                DecryptedText[i] = static_cast<char>(j);
+                break;
+            }
+        }
+    }
+}
+
+void CaesarCipher::Shift(int shift)
+{
+    for (int i = 0; i < ASCII; i++)
+    {
+        Key[i] = static_cast<char>(i);
+    }
+    shift %= ASCII;
+    std::rotate(Key.begin(), Key.begin() + (ASCII - shift), Key.end());
+}
---
+++ Caesar.h
@@ -0,0 +1,20 @@
+﻿#pragma 
+
+#include <D:\\Visual Studio\\С++\\ProgSystemDefending\\CryptoClasses\\Crypto.h>
+
+class CaesarCipher : public Crypto
+{
+private:
+    std::string Key;
+    int shift;
+    static const int ASCII = 256; 
+
+public:
+    CaesarCipher(int s);
+
+    void Encrypt(const std::string& OriginalText, std::string& EncryptedText) override;
+    void Decrypt(const std::string& EncryptedText, std::string& DecryptedText) override;
+
+private:
+    void Shift(int shift);
+};
